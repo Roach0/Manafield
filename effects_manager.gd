c2efe1b29a17fa39885749517f6289f9a66672f4
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2026-06-11 16:07:32 -0500

    0.03

--- effects_manager.gd
+++ effects_manager.gd
@@ -1,5 +1,12 @@
 extends HBoxContainer
 class_name EffectsManager
 
-@onready var right_panel: RightPanel = $RightPanel
-@onready var world: World = $World
+@onready var right_panel: RightPanel = %RightPanel
+@onready var left_panel: LeftPanel = %LeftPanel
+
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
